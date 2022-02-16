ffda7cb96d49dcf39ad9c6614ac1dbdcfad3777f
MattIPv4 <me@mattcowley.co.uk>
2022-02-16 01:29:08 +0000

    Remove cell level concept, allow hazard cells to be explored

--- src/move.js
+++ src/move.js
@@ -1,4 +1,4 @@
-const { cellLevels, logLevels } = require('./utils/enums');
+const { logLevels } = require('./utils/enums');
 const floodFill = require('./utils/floodFill');
 const log = require('./utils/log');
 const { adjust, surrounding } = require('./utils/position');
@@ -10,24 +10,28 @@ const scoreSpace = (data, grid, pos, wrap) => {
     // TODO: Consider where other snakes could move to when ranking the space for this cell
     let open = 0;
     floodFill(grid, pos, (grid, x, y) => {
-        // Track open cells we found
-        if (grid[x][y].level >= cellLevels.empty) open++;
-
         // Don't explore from bad cells
-        if (grid[x][y].level < cellLevels.empty) return { continue: true };
+        if (grid[x][y].snake) return { continue: true };
+
+        // Track open cells we found (treat hazard cells as open, but give them far less weight)
+        open += grid[x][y].hazard ? 1/5 : 1;
     }, wrap);
 
-    return { score: open / (grid.length * grid[0].length), data: {} };
+    return {
+        score: open / (grid.length * grid[0].length),
+        data: {},
+    };
 };
 
 const scoreFood = (data, grid, pos, wrap) => {
     // Find nearest food
+    // TODO: Allow exploring past hazard cells, adding a penalty for each cell
     const foodPos = floodFill(grid, pos, (grid, x, y) => {
-        // If we find food, return it
-        if (grid[x][y].level === cellLevels.food) return { return: { x, y } };
-
         // Don't explore from bad cells
-        if (grid[x][y].level < cellLevels.empty) return { continue: true };
+        if (grid[x][y].snake) return { continue: true };
+
+        // If we find food, return it
+        if (grid[x][y].food) return { return: { x, y } };
     }, wrap);
     const manhattan = foodPos ? Math.abs(pos.x - foodPos.x) + Math.abs(pos.y - foodPos.y) : 0;
 
@@ -35,7 +39,14 @@ const scoreFood = (data, grid, pos, wrap) => {
     const food = exponential(manhattan / (grid.length + grid[0].length), 5, true);
     const health = exponential(data.you.health / 100, 5, true);
 
-    return { score: food * health, data: { manhattan, food, health } };
+    return {
+        score: food * health,
+        data: {
+            manhattan,
+            food,
+            health,
+        },
+    };
 };
 
 const scoreHeadToHead = (data, grid, pos, wrap) => {
@@ -44,7 +55,7 @@ const scoreHeadToHead = (data, grid, pos, wrap) => {
         const cellAdjusted = adjust(cell, grid, wrap);
         if (!cellAdjusted) return false;
 
-        if (grid[cellAdjusted.x][cellAdjusted.y].level !== cellLevels.snake) return false;
+        if (!grid[cellAdjusted.x][cellAdjusted.y].snake) return false;
         if (grid[cellAdjusted.x][cellAdjusted.y].snake.id === data.you.id) return false;
         if (grid[cellAdjusted.x][cellAdjusted.y].snake.head.x !== cellAdjusted.x) return false;
         if (grid[cellAdjusted.x][cellAdjusted.y].snake.head.y !== cellAdjusted.y) return false;
@@ -54,18 +65,22 @@ const scoreHeadToHead = (data, grid, pos, wrap) => {
     });
 
     // Don't subtract 1 from `data.snakes.length` to account for self, as we never want this to equal 0
-    return { score: 1 - (dangerousHeads.length / data.board.snakes.length), data: {} };
+    return {
+        score: 1 - (dangerousHeads.length / data.board.snakes.length),
+        data: {
+            potential: dangerousHeads.length,
+        },
+    };
 };
 
 const scoreMove = (data, grid, pos, wrap) => {
     // Adjust for out-of-bounds
     const adjusted = adjust(pos, grid, wrap);
-    if (!adjusted) return { score: 0, scoreData: 'out-of-bounds' };
+    if (!adjusted) return { score: 0, data: { msg: 'cell is out of bounds' } };
     const { x, y } = adjusted;
 
-    // Avoid hazards and snakes
-    if (grid[x][y].level < cellLevels.empty)
-        return { score: 0, scoreData: `cell marked ${cellLevels[grid[x][y].level]}` };
+    // Avoid any snakes at all costs
+    if (grid[x][y].snake) return { score: 0, data: { msg: 'cell contains snake' } };
 
     // Get all the raw scores to consider
     const scores = [
@@ -82,9 +97,17 @@ const scoreMove = (data, grid, pos, wrap) => {
             ...scoreFood(data, grid, adjusted, wrap),
         },
         {
+            // Try to avoid potential head-to-heads we can't win, but allow them if we must
             name: 'head-to-head',
             type: 'mult',
             ...scoreHeadToHead(data, grid, adjusted, wrap),
+        },
+        {
+            // Try to avoid hazard cells if we can, but allow them to be used if we absolutely must
+            name: 'hazard',
+            type: 'mult',
+            score: grid[x][y].hazard ? 0.1 : 1,
+            data: grid[x][y].hazard ? { msg: 'cell is hazardous' } : {},
         },
     ];
 
@@ -112,7 +135,7 @@ module.exports = data => {
     // Create the empty grid
     const grid = Array(data.board.width).fill(null)
         .map(() => Array(data.board.height).fill(null)
-            .map(() => ({ level: cellLevels.empty })));
+            .map(() => ({})));
 
     // Check what gamemode we're running
     const wrap = data.game.ruleset.name === 'wrapped';
@@ -124,23 +147,15 @@ module.exports = data => {
         for (const [ index, part ] of snake.body.entries()) {
             // If not constricting, ignore the tail as it will be gone during this tick
             if (!constrict && index === snakeLength - 1) continue;
-
-            grid[part.x][part.y].level = cellLevels.snake;
             grid[part.x][part.y].snake = snake;
         }
     }
 
     // Track all hazards on the board
-    for (const hazard of data.board.hazards) {
-        grid[hazard.x][hazard.y].level = cellLevels.hazard;
-        grid[hazard.x][hazard.y].hazard = hazard;
-    }
+    for (const hazard of data.board.hazards) grid[hazard.x][hazard.y].hazard = hazard;
 
     // Track all the food on the board
-    for (const food of data.board.food) {
-        grid[food.x][food.y].level = cellLevels.food;
-        grid[food.x][food.y].food = food;
-    }
+    for (const food of data.board.food) grid[food.x][food.y].food = food;
 
     // Score each move from current position
     const moves = surrounding(data.you.head).map(move => ({ ...move, ...scoreMove(data, grid, move, wrap) }))
--- src/utils/enums.js
+++ src/utils/enums.js
@@ -3,13 +3,6 @@ logLevels[logLevels.debug = 0] = 'debug';
 logLevels[logLevels.warn = 1] = 'warn';
 logLevels[logLevels.error = 2] = 'error';
 
-const cellLevels = {};
-cellLevels[cellLevels.hazard = -2] = 'hazard';
-cellLevels[cellLevels.snake = -1] = 'snake';
-cellLevels[cellLevels.empty = 0] = 'empty';
-cellLevels[cellLevels.food = 1] = 'food';
-
 module.exports = {
   logLevels,
-  cellLevels,
 };
